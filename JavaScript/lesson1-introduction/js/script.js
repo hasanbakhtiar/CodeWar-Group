e4856ec7d539345829a7149ee46472e47bdc6612
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-05 15:39:49 +0400

    js enter

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,31 @@
+// one line
+/* more line
+comment
+*/
+
+
+// Primitive DT 
+// string
+// number
+// boolean
+
+// Composite DT
+// array
+// var info = [5, "Hello", true];
+// console.log(info);
+
+// object
+// var info = {
+//     a:"JavaScript",
+//     b:"Browser"
+// }
+// console.log(info.a);
+
+
+// Special
+// undefined
+// var info = undefined;
+// console.log(info);
+// null 
+// var info = null;
+// console.log(info);
