fe3e986853dbfe5c4e531cd0ff8ca1868f0c8781
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-12 15:27:46 +0400

    condition

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,85 @@
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+// string operators
+// +
+// +=
+
+
+
+// var infoOne = "Javascript";
+// infoOne += " Hello";
+
+// console.log(infoOne);
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5,  b= 10
+// console.log(a,b);
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var info = 'JavaScript\'s Book';
+// console.log(info); 
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+var a = 10;
+
+console.log(void(a));
+
+
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a = 5;
+
+
+
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+
+
+
+
---
+++ JavaScript/lesson4-variables-conditions/js/script.js
@@ -0,0 +1,23 @@
+// if -> eger 
+// else -> deyilse  
+// else if -> deyilse eger 
+
+
+
+
+
+
+// var currentTime = new Date().getHours();
+
+// if (currentTime > 5 && currentTime <11 ) {
+//     document.write('good morning');
+// }else if(currentTime >= 11 && currentTime <18 ){
+//     document.write('good afternoon');
+// }else if(currentTime >=18 && currentTime <21 ){
+//     document.write('good evening');
+// }else if(currentTime >= 21 && currentTime <= 23 ){
+//     document.write('good night');
+// }
+// else{
+//     document.write('no time');
+// }
