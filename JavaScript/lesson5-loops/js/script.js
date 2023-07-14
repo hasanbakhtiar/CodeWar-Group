4b180769f9583754071750a489e1654c4e712679
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-14 14:56:26 +0400

    loops

---
+++ JavaScript/lesson5-loops/js/script.js
@@ -0,0 +1,49 @@
+// let info = 0;
+// while (info > 10) {
+//     console.log("Hello" + info);
+//     info++;
+// }
+
+
+// let info = 0;
+// do{
+//     console.log(info);
+//     info++;
+// }while(info>10);
+
+
+// for (let info = 0; info < 10; info++) {
+//     console.log(info);
+// }
+
+
+
+
+// const info = "Asus";
+// const price = 1000
+
+// console.log(info+" Price:"+price+"$");
+// console.log(`${info} hellojaksdhaksdhak: ${price}`);
+
+// const worktime = 10;
+
+
+
+// const productOne = ['bread','water','oil']; //10
+// const productTwo = ['onion','milk','egg']; //20
+// const productThree = ['meat','cola','fruits','fish']; //40
+// const productFour = productOne + productTwo; //70
+// const productFive = productTwo+ productThree //120
+
+
+for (let count = 0; count < 20; count++) {
+    if (count === 10) {
+        // break;
+        continue;
+    }
+    console.log(count);
+    
+}
+
+
+
