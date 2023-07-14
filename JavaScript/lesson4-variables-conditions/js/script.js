95ad7552cde457a6eb267d43e74f5fa6333ee48b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-14 17:47:29 +0400

    loops

--- JavaScript/lesson4-variables-conditions/js/script.js
+++ JavaScript/lesson4-variables-conditions/js/script.js
@@ -21,3 +21,85 @@
 // else{
 //     document.write('no time');
 // }
+
+
+
+
+
+
+// var age = 200;
+
+// switch (age) {
+//     case 20:
+//         console.log('this is 20');
+//         break;
+//     case 18:
+//         console.log('access');
+//         break;
+//     default:
+//         console.log('this is default');
+
+//         break;
+// }   
+
+
+
+
+
+
+
+
+
+// global
+
+// if (true) {
+//     // local global
+//     if (true) {
+//         // local global
+//         let a = 5;
+//         if (true) {
+//             // local
+//         }
+//     }
+// }
+
+// console.log(a);
+
+
+
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var      -> variable (global)
+// const    -> constant (local)
+// let      -> let      (local)
+
+
+const abstract = "Hello";
+console.log(abstract);
+
+
