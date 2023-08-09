94545257f6693da579d5323a50174fbf3af03798
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-08-09 15:21:02 +0400

    dom style

--- JavaScript/lesson11-DOM-part2/js/script.js
+++ JavaScript/lesson11-DOM-part2/js/script.js
@@ -14,11 +14,69 @@
 
 
 
-const listBtn = document.querySelector('.list-nd')
-const submenu = document.querySelector('.submenu')
+// const listBtn = document.querySelector('.list-nd')
+// const submenu = document.querySelector('.submenu')
 
 
-const subMenuChanged = ()=>{
-    submenu.style.height = '130px';
+// const subMenuChanged = ()=>{
+//     if (submenu.style.height == "0px") {
+//         submenu.style.height = "130px"
+//     }else{
+//         submenu.style.height = "0px";
+//     }
+// }
+// listBtn.onclick = subMenuChanged;
+
+
+
+// document.querySelector('button').onclick = () => { document.querySelector('ol').style.transform = "translate(0)" }
+
+// const btn = document.querySelector('button');
+// const icon = document.querySelector('i');
+
+// const toggle = () => { 
+//     if (icon.attributes[0].value === "fa-solid fa-bars") {
+//         document.querySelector('ol').style.transform = "translate(0)" 
+//        icon.attributes[0].value = 'fa-solid fa-angles-left'
+//     }else{
+//         document.querySelector('ol').style.transform = "translate(-300px)" 
+//         icon.attributes[0].value = 'fa-solid fa-bars'
+//     }
+// }
+
+// btn.onclick = toggle;
+
+
+const userdata = {
+    email:"info@text.com",
+    password:"test123"
 }
-listBtn.onclick = subMenuChanged;
+
+const emailInput = document.querySelectorAll('input')[0];
+const passInput = document.querySelectorAll('input')[1];
+const alert = document.querySelector('#alert');
+const btn = document.querySelector('button');
+
+
+btn.onclick=()=>{
+     if (!emailInput.value || !passInput.value) {
+        alert.innerHTML = "please, fill input"
+            alert.className = "alert alert-warning";
+     }else{
+        if (emailInput.value === userdata.email && passInput.value === userdata.password) {
+            // alert.innerHTML = "login successfully"
+            // alert.className = "alert alert-success";
+            window.location.replace("admin.html");
+        }else{
+            alert.innerHTML = "password or email is wrong!"
+            alert.className = "alert alert-danger";
+
+        }
+     }
+}
+    
+
+
+
+
+
