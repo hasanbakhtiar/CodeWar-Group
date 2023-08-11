519f60be7901230b6ae6d5b315a85ead20e5e237
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-08-11 15:04:42 +0400

    todo app

--- JavaScript/lesson12-DOM-part3-todoapp/js/script.js
+++ JavaScript/lesson12-DOM-part3-todoapp/js/script.js
@@ -1,12 +1,18 @@
 const input = document.querySelector('input');
-const btn = document.querySelector('#add');
+const form = document.querySelector('form');
 const listCon = document.querySelector('.list-group');
+const clearAll = document.querySelector('#clearAll');
 
-const todoList = () => {
+const todoList = (e) => {
+    e.preventDefault();
+   if (!input.value) {
+    alert('please, fill input')
+   }else{
     const newLi = document.createElement('li');
     const newText = `<span class="list-text">${input.value}</span><button class="btn btn-danger mybtn">X</button>`;
     newLi.innerHTML = newText;
     newLi.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center')
+    newLi.setAttribute('data-aos', 'zoom-out-right')
     listCon.append(newLi);
     input.value = "";
 
@@ -14,17 +20,33 @@ const todoList = () => {
     for (let i = 0; i < li.length; i++) {
         li[i].addEventListener('click',() => {
             document.querySelectorAll('.list-text')[i].style.textDecoration = "line-through";
-
+           
         },true)
     }
+    for (let i = 0; i < li.length; i++) {
+        li[i].addEventListener('dblclick',() => {
+            document.querySelectorAll('.list-text')[i].style.textDecoration = "none";
+           
+        },true)
+    }
+    
      for (let i = 0; i < li.length; i++) {
         document.querySelectorAll('.mybtn')[i].onclick = () => {
            li[i].remove();
 
         }
     }
+   }
 }
 
-btn.onclick = todoList;
+form.onsubmit = todoList;
+
+clearAll.onclick = ()=>{
+    const li = document.querySelectorAll('li');
+    for (let i = 0; i < li.length; i++) {
+           li[i].remove();
+
+    }
+}
 
 
