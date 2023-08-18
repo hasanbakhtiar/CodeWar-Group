752a7fe4e7d081d96c5ba885d463b4686db5a565
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-08-18 14:57:05 +0400

    api

--- JavaScript/lesson14-api/js/script.js
+++ JavaScript/lesson14-api/js/script.js
@@ -1,13 +1,12 @@
-let selectCategory = "smartphones";
 
-const productApi = ()=>{
+const productApi = (selectCategory) => {
     fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then(data => {
-        let col = "";
-        data.products.map(item => {
-            if (item.category === selectCategory) {
-                col += `
+        .then(res => res.json())
+        .then(data => {
+            let col = "";
+            data.products.map(item => {
+                if (!selectCategory) {
+                    col += `
             <div class="col-12 col-sm-12 col-md-3">
             <div class="card" >
             <img src="${item.images[0]}" height="300" class="card-img-top" alt="...">
@@ -19,26 +18,39 @@ const productApi = ()=>{
           </div>
             </div>
               `
-            }
+                } else {
+                    if (item.category === selectCategory) {
+                        col += `
+                <div class="col-12 col-sm-12 col-md-3">
+                <div class="card" >
+                <img src="${item.images[0]}" height="300" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${item.title}</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+                </div>
+                  `
+                    }
+                }
+            })
+            document.querySelector('.row').innerHTML = col;
         })
-        document.querySelector('.row').innerHTML = col;
-    })
 }
+
+
+
+document.querySelectorAll('button')[0].onclick = () => {
+    productApi("smartphones");
+}
+
+document.querySelectorAll('button')[1].onclick = () => {
+    productApi("laptops");
+}
+
+document.querySelectorAll('button')[2].onclick = () => {
+    productApi("fragrances");
+}
+
 productApi();
-
-
-
-document.querySelectorAll('button')[0].onclick =()=>{
-        selectCategory = "smartphones";
-        productApi();
-}
-
-document.querySelectorAll('button')[1].onclick =()=>{
-    selectCategory = "laptops";
-    productApi();
-}
-
-document.querySelectorAll('button')[2].onclick =()=>{
-    selectCategory = "fragrances";
-    productApi();
-}
